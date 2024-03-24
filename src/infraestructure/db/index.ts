ffd8398c4f578183bce2e8f78b8a3a72020dd79f
juanuranidev <juanuranidev@gmail.com>
2024-03-23 23:48:53 -0300

    Working on db

---
+++ src/infraestructure/db/index.ts
@@ -0,0 +1,12 @@
+import ENVS from "../../config/envs/envs";
+import * as schema from "./schemas/index";
+import { Client } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+const client = new Client({
+  connectionString: ENVS.POSTGRES_URL,
+});
+
+client.connect();
+
+export const db = drizzle(client, { schema });
---
+++ src/infraestructure/db/migrate.ts
@@ -0,0 +1,28 @@
+import ENVS from "../../config/envs/envs";
+import { Pool } from "pg";
+import { migrate } from "drizzle-orm/node-postgres/migrator";
+import { drizzle } from "drizzle-orm/node-postgres";
+import * as schema from "./schemas/index";
+
+const pool = new Pool({
+  connectionString: ENVS.POSTGRES_URL,
+});
+
+const db = drizzle(pool, { schema });
+
+async function main() {
+  try {
+    console.log("Migrations started ⚠️");
+    await migrate(db, {
+      migrationsFolder: "src/infraestructure/db/migrations",
+      migrationsTable: "migrations",
+    });
+
+    console.log("Migrations ended correctly ✅");
+    process.exit();
+  } catch (error) {
+    console.log("Error in migrations 🚫:", error);
+  }
+}
+
+main();
