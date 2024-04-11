c92bf3cb3811c495632feeeba7ed856b43cf317e
juanuranidev <juanuranidev@gmail.com>
2024-04-10 23:58:40 -0300

    Updated envs and env template

--- src/config/envs/envs.ts
+++ src/config/envs/envs.ts
@@ -1,7 +1,14 @@
 import "dotenv/config";
 import { get } from "env-var";
 
-export const envs = {
+const ENVS = {
   PORT: get("PORT").required().asPortNumber(),
   NODE_ENV: get("NODE_ENV").required().asString(),
+  POSTGRES_USER: get("POSTGRES_USER").required().asString(),
+  POSTGRES_DB: get("POSTGRES_DB").required().asString(),
+  POSTGRES_HOST: get("POSTGRES_HOST").required().asString(),
+  POSTGRES_PASSWORD: get("POSTGRES_PASSWORD").required().asString(),
+  POSTGRES_URL: get("POSTGRES_URL").required().asString(),
 };
+
+export default ENVS;
