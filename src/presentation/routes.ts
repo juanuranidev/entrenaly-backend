ac6b11a05e134d19af3f41b10215e84f8c29cd33
juanuranidev <juanuranidev@gmail.com>
2024-06-04 23:52:35 -0300

    Updated some routes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { UserRoutes } from "./user/routes";
+import { PlanRoutes } from "./plan/routes";
+import { ClientRoutes } from "./client/routes";
 import { ExerciseRoutes } from "./exercise/routes";
 
 export class AppRoutes {
@@ -8,6 +10,8 @@ export class AppRoutes {
 
     router.use("/api/users", UserRoutes.routes);
     router.use("/api/exercise", ExerciseRoutes.routes);
+    router.use("/api/client", ClientRoutes.routes);
+    router.use("/api/plan", PlanRoutes.routes);
 
     return router;
   }
---
+++ src/presentation/user/controller.ts
@@ -0,0 +1,87 @@
+import { CreateGoogleUserDto } from "../../domain/dtos/user/create-google-user.dto";
+import { Request, Response } from "express";
+import { UserRepository } from "../../domain/repositories/user/user.repository";
+import { CreateUserDto } from "../../domain/dtos/user/create-user.dto";
+import { CustomError } from "../../domain/errors/custom.error";
+
+export class UserController {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  private handleError = (error: unknown, res: Response) => {
+    if (error instanceof CustomError) {
+      return res.status(error.statusCode).json({ error: error.message });
+    }
+    return res.status(500).json({ error: "Internal server error" });
+  };
+
+  public createUser = async (req: Request, res: Response) => {
+    try {
+      const { data } = req.body;
+
+      const [error, registerUserDto] = CreateUserDto.create(data);
+      if (error) {
+        return res.status(400).json({ error });
+      }
+
+      const userRegistered = await this.userRepository.createUser(
+        registerUserDto!
+      );
+
+      return res.status(201).json(userRegistered);
+    } catch (error) {
+      console.log(error);
+      this.handleError(error, res);
+    }
+  };
+
+  public createUserWithGoogleAuth = async (req: Request, res: Response) => {
+    try {
+      const { data } = req.body;
+
+      const [error, googleUserDto] = CreateGoogleUserDto.create(data);
+      if (error) {
+        return res.status(400).json({ error });
+      }
+
+      const user = await this.userRepository.createUserWithGoogleAuth(
+        googleUserDto!
+      );
+
+      return res.status(201).json(user);
+    } catch (error) {
+      console.log(error);
+      this.handleError(error, res);
+    }
+  };
+  public readUserByAuthId = async (req: Request, res: Response) => {
+    try {
+      const { authId } = req.query;
+      if (!authId) {
+        return this.handleError("authId not given", res);
+      }
+
+      const user = await this.userRepository.readUserByAuthId(String(authId));
+
+      return res.status(200).json(user);
+    } catch (error) {
+      console.log(error);
+      this.handleError(error, res);
+    }
+  };
+  public getUser = async (req: Request, res: Response) => {
+    try {
+      const { id: trainerId } = req.body.user;
+      if (!trainerId) {
+        console.log("A");
+        return this.handleError("trainerId not given", res);
+      }
+
+      const user = await this.userRepository.readUser(String(trainerId));
+
+      return res.status(200).json(user);
+    } catch (error) {
+      console.log(error);
+      this.handleError(error, res);
+    }
+  };
+}
