aff229b24999fb3b724d67ce6d124be088e5bb34
juanuranidev <juanuranidev@gmail.com>
2024-06-10 23:06:35 -0300

    Updates to deploy the project

---
+++ src/infraestructure/db/migrations/0000_large_crystal.sql
@@ -0,0 +1,228 @@
+CREATE TABLE IF NOT EXISTS "clients" (
+	"id" varchar(256) PRIMARY KEY NOT NULL,
+	"user_id" varchar NOT NULL,
+	"is_active" boolean DEFAULT true NOT NULL,
+	"trainer_id" varchar NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"weight" varchar(256),
+	"height" varchar(256),
+	"goals" varchar(256),
+	"injuries" varchar(256),
+	"medicalConditions" varchar(256)
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "invites" (
+	"id" varchar(256) PRIMARY KEY NOT NULL,
+	"user_id" varchar NOT NULL,
+	CONSTRAINT "invites_user_id_unique" UNIQUE("user_id")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "exercises" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(256) NOT NULL,
+	"video" varchar(256),
+	"image" varchar(256),
+	"category_id" integer NOT NULL,
+	"user_id" varchar,
+	"is_active" boolean DEFAULT true NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "exercises_categories" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(256) NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "exercises_descriptions" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(256) NOT NULL,
+	"user_id" varchar NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "variants" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(256) NOT NULL,
+	"video" varchar(256),
+	"image" varchar(256),
+	"category_id" integer NOT NULL,
+	"user_id" varchar NOT NULL,
+	"exercise_id" integer NOT NULL,
+	"is_active" boolean DEFAULT true NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "roles" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(256) NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "subscription_plans" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(256) NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" varchar(256) PRIMARY KEY NOT NULL,
+	"name" varchar(256) NOT NULL,
+	"email" varchar(256) NOT NULL,
+	"image" varchar(256),
+	"auth_id" varchar(256) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"role_id" integer NOT NULL,
+	"subscription_plan" integer NOT NULL,
+	CONSTRAINT "users_id_unique" UNIQUE("id"),
+	CONSTRAINT "users_email_unique" UNIQUE("email"),
+	CONSTRAINT "users_auth_id_unique" UNIQUE("auth_id")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "clients_plans" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"client_id" varchar NOT NULL,
+	"plan_id" varchar NOT NULL,
+	"is_active" boolean DEFAULT true NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "days_of_week" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(256) NOT NULL,
+	"order" integer NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "plans" (
+	"id" varchar(256) PRIMARY KEY NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"is_active" boolean DEFAULT true NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"name" varchar(256) NOT NULL,
+	"category_id" integer NOT NULL,
+	"type_id" integer NOT NULL,
+	"user_id" varchar NOT NULL,
+	CONSTRAINT "plans_id_unique" UNIQUE("id")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "plans_categories" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(256) NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "plans_days" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"plan_id" varchar NOT NULL,
+	"day_of_week_id" integer NOT NULL,
+	"is_active" boolean DEFAULT true NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "plans_exercises" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"plan_day_id" integer NOT NULL,
+	"exercise_id" integer NOT NULL,
+	"description" varchar(256) NOT NULL,
+	"is_active" boolean DEFAULT true NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "plans_types" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(256) NOT NULL
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "exercises" ADD CONSTRAINT "exercises_category_id_exercises_categories_id_fk" FOREIGN KEY ("category_id") REFERENCES "exercises_categories"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "exercises" ADD CONSTRAINT "exercises_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "exercises_descriptions" ADD CONSTRAINT "exercises_descriptions_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "variants" ADD CONSTRAINT "variants_category_id_exercises_categories_id_fk" FOREIGN KEY ("category_id") REFERENCES "exercises_categories"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "variants" ADD CONSTRAINT "variants_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "variants" ADD CONSTRAINT "variants_exercise_id_exercises_id_fk" FOREIGN KEY ("exercise_id") REFERENCES "exercises"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "users" ADD CONSTRAINT "users_role_id_roles_id_fk" FOREIGN KEY ("role_id") REFERENCES "roles"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "users" ADD CONSTRAINT "users_subscription_plan_subscription_plans_id_fk" FOREIGN KEY ("subscription_plan") REFERENCES "subscription_plans"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "clients_plans" ADD CONSTRAINT "clients_plans_client_id_clients_id_fk" FOREIGN KEY ("client_id") REFERENCES "clients"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "clients_plans" ADD CONSTRAINT "clients_plans_plan_id_plans_id_fk" FOREIGN KEY ("plan_id") REFERENCES "plans"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "plans" ADD CONSTRAINT "plans_category_id_plans_categories_id_fk" FOREIGN KEY ("category_id") REFERENCES "plans_categories"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "plans" ADD CONSTRAINT "plans_type_id_plans_types_id_fk" FOREIGN KEY ("type_id") REFERENCES "plans_types"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "plans" ADD CONSTRAINT "plans_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "plans_days" ADD CONSTRAINT "plans_days_plan_id_plans_id_fk" FOREIGN KEY ("plan_id") REFERENCES "plans"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "plans_days" ADD CONSTRAINT "plans_days_day_of_week_id_days_of_week_id_fk" FOREIGN KEY ("day_of_week_id") REFERENCES "days_of_week"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "plans_exercises" ADD CONSTRAINT "plans_exercises_plan_day_id_plans_days_id_fk" FOREIGN KEY ("plan_day_id") REFERENCES "plans_days"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "plans_exercises" ADD CONSTRAINT "plans_exercises_exercise_id_exercises_id_fk" FOREIGN KEY ("exercise_id") REFERENCES "exercises"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
