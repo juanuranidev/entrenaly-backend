4074a053ee46f8d88b6d4b035944eb2ca669e205
juanuranidev <juanuranidev@gmail.com>
2024-08-29 19:18:25 -0300

    Updated drizzle drizzle-kit and drizzle-orm libraries

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,15 @@
 import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
 import ENVS from "./src/config/envs/envs";
 
-export default {
-  schema: "src/infraestructure/db/schemas/index.ts",
-  out: "src/infraestructure/db/migrations",
-  driver: "pg",
+export default defineConfig({
+  schema: "./src/infraestructure/db/schemas/index.ts",
+  out: "./src/infraestructure/db/migrations",
+  dialect: "postgresql",
   dbCredentials: {
     host: ENVS.POSTGRES_PORT,
     user: ENVS.POSTGRES_USER,
     password: ENVS.POSTGRES_PASSWORD,
     database: ENVS.POSTGRES_DB,
   },
-};
+});
